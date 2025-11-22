068d61653dfebc60c5ad2c074088f27bb5bd7fb2
Themal Lakruwan <themaldesilva2001@gmail.com>
2025-11-22 22:52:04 +0530

    Intilized service. created entity classes and DTOs

---
+++ reservation-management-service/src/main/java/com/reservation/reservation_management_service/dto/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.reservation.reservation_management_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ApiResponse<T> {
+
+    private boolean success;
+    private String message;
+    private T data;
+    private String error;
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(String error, String message) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .error(error)
+                .build();
+    }
+}
