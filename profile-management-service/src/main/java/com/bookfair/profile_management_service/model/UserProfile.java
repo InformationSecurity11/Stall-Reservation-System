e24989976ac101ae8156a64e9099cb5b4b37830d
uvishka <uvishka@gmail.com>
2025-11-23 11:10:55 +0530

    Added MongoDB and Finalize the Profile Management

--- profile-management-service/src/main/java/com/bookfair/profile_management_service/model/UserProfile.java
+++ profile-management-service/src/main/java/com/bookfair/profile_management_service/model/UserProfile.java
@@ -1,38 +1,33 @@
 package com.bookfair.profile_management_service.model;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
-@Entity
+@Document(collection = "profiles") 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "profiles")
 public class UserProfile {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id; 
 
-    // Links to the Auth Service Login ID
     private String userId; 
-
-    // Basic Info
     private String fullName;
     private String email;
     private String phoneNumber;
 
-    // Vendor Specific Info 
+    // Vendor Info
     private String companyName;
     private String businessRegNo;
     private String address;
 
-    // Literary Genres (e.g., "Fiction", "Sci-Fi") 
-    @ElementCollection
+    // Genres 
     private List<String> literaryGenres;
 
-    // Role: "VENDOR" or "ADMIN" (Employee) [cite: 34]
     private String role; 
 }
